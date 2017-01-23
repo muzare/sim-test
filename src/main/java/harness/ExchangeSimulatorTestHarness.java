e8e7de91abe146cdd811f34092642ba2d9906b15
adam myers <adam@localhost.localdomain>
2017-01-22 21:19:58 -0600

    Added documentation to Harness/TestStep.

--- src/main/java/harness/ExchangeSimulatorTestHarness.java
+++ src/main/java/harness/ExchangeSimulatorTestHarness.java
@@ -1,5 +1,6 @@
 package harness;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import org.junit.After;
 import org.junit.Rule;
@@ -8,7 +9,7 @@ import simulator.ExchangeSimulatorRuntime;
 import java.util.List;
 
 /**
- * Created by adam on 1/22/17.
+ * Test harness for writing test cases against the {@link simulator.ExchangeSimulator}.
  */
 public abstract class ExchangeSimulatorTestHarness {
     private final List<ExchangeSimulatorTestStep> testSteps = Lists.newArrayList();
@@ -19,15 +20,28 @@ public abstract class ExchangeSimulatorTestHarness {
     @Rule
     public final ExchangeSimulatorRuntime simulatorRuntime = new ExchangeSimulatorRuntime();
 
+    /**
+     * Resets all test steps registered by the running test case.
+     */
     @After
     public final void clearTestSteps() {
         testSteps.clear();
     }
 
+    /**
+     * Registers an {@link ExchangeSimulatorTestStep} for execution. Test steps are processed in FIFO order.
+     *
+     * @param testStep The test step to perform (cannot be null).
+     * @throws IllegalArgumentException if the parameter conditions are not met.
+     */
     protected final void registerTestStep(final ExchangeSimulatorTestStep testStep) {
+        Preconditions.checkArgument(testStep != null, "Cannot register a null test step.");
         testSteps.add(testStep);
     }
 
+    /**
+     * Performs all {@link #registerTestStep(ExchangeSimulatorTestStep) registered test steps} in FIFO order.
+     */
     protected final void performTestSteps() {
         testSteps.forEach(step -> step.performStep());
     }
--- src/main/java/harness/ExchangeSimulatorTestStep.java
+++ src/main/java/harness/ExchangeSimulatorTestStep.java
@@ -1,5 +1,6 @@
 package harness;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import org.hamcrest.Matcher;
 import simulator.SimulationResults;
@@ -12,31 +13,62 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 
 /**
- * Created by adam on 1/22/17.
+ * Represents a test step for the {@link simulator.ExchangeSimulator}. Extending classes should re=implement the members of this class to provide the expected input
+ * to the similar and the appropriate assertions on the output.
  */
 public abstract class ExchangeSimulatorTestStep {
+
     private final ExchangeSimulatorTestHarness exchangeSimulatorTestHarness;
 
+    /**
+     * Constructs a new {@link ExchangeSimulatorTestStep}
+     *
+     * @param exchangeSimulatorTestHarness The {@link ExchangeSimulatorTestHarness} for the test class (cannot be null).
+     * @throws IllegalArgumentException if the parameter conditions are not met.
+     */
     public ExchangeSimulatorTestStep(final ExchangeSimulatorTestHarness exchangeSimulatorTestHarness) {
+        Preconditions.checkArgument(exchangeSimulatorTestHarness != null, "Cannot instantiate a test step with a null test harness.");
         this.exchangeSimulatorTestHarness = exchangeSimulatorTestHarness;
     }
 
+    /**
+     * Extending classes should implement this to populate the {@link Order} objects in the sequence that they should be provided to the simulator.
+     *
+     * @param orderListBuilder A non-null ImmutableList.Builder to populate.
+     */
     protected abstract void populateOrders(final ImmutableList.Builder<Order> orderListBuilder);
 
+    /**
+     * Extending classes should implement this to return the Matchers for the expected {@link Trade}s output by the simulator. By default, no trades are expected.
+     *
+     * @return Matcher for the expected {@link Trade}s output by the simulator.
+     */
     protected Matcher<Trade>[] getTradeMatchers() {
         return new Matcher[]{};
     }
 
+    /**
+     * Extending classes should implement this to return the Matchers for the expected warnings output by the simulator. By default, no warnings are expected.
+     *
+     * @return Matcher for the expected {warningss output by the simulator.
+     */
     protected Matcher<String>[] getWarningMatchers() {
         return new Matcher[]{};
     }
 
+    /**
+     * Extending classes should implement this to indicate whether the process is expected to abort upon receiving the input specified in {@link #populateOrders(ImmutableList.Builder)}.
+     * By default, the process is not expected to abort.
+     *
+     * @return True if the simulation should abort after receiving the input Orders, false otherwise.
+     */
     protected boolean shouldProcessAbort() {
         return false;
     }
 
     /**
-     * Performs the test. The test steps
+     * Performs the test step, first sending the orders populated via {@link #populateOrders(ImmutableList.Builder)} to the {@link simulator.ExchangeSimulator}, then asserting the
+     * {@link SimulationResults} according to the test step implementation.
      */
     protected final void performStep() {
         final ImmutableList.Builder orderListBuilder = ImmutableList.builder();
